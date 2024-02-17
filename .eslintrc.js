8fb86d2c2e2dd5fadd8981fc1a71e0a3df02ab32
Yeldos Sergali <max.boostvink@gmail.com>
2024-02-17 10:41:21 +0600

    First Commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,25 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    es2021: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
+    // Основной набор правил ESLint
     "eslint:recommended",
+    // Правила ESLint специфичные для Vue.js
+    "plugin:vue/vue3-essential", // Или 'plugin:vue/vue3-strongly-recommended' или 'plugin:vue/vue3-recommended'
+    // Правила для поддержки синтаксиса ES6+
+    "@vue/eslint-config-typescript/recommended",
+    // Добавьте это, если используете Prettier для форматирования кода
     "plugin:prettier/recommended",
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    ecmaVersion: 2021,
+    parser: "@typescript-eslint/parser", // Используйте, если ваш проект на TypeScript
   },
   rules: {
+    // Здесь вы можете переопределить или добавить свои правила
+    // Например, отключить правило, которое не позволяет использовать console.log
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
 };
---
+++ src/components/MyHello.vue
@@ -0,0 +1,4 @@
+<template>
+  <p>This is my component!</p>
+  <button>Open</button>
+</template>
